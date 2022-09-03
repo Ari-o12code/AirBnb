add2a3f4335a1010705e9b1e674ddc512d1dd471
Quadri Alameen <quadrameen@gmail.com>
2022-09-03 22:07:13 +0100

    Updates

---
+++ precache-manifest.855cd1839079d211229a37e33dd5be1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21476903835920d58b9",
+    "url": "/AirBnb2/static/css/main.faec0dd5.chunk.css"
+  },
+  {
+    "revision": "a21476903835920d58b9",
+    "url": "/AirBnb2/static/js/main.a2147690.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/AirBnb2/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "6977315240f450f9ff86",
+    "url": "/AirBnb2/static/css/2.269cb199.chunk.css"
+  },
+  {
+    "revision": "6977315240f450f9ff86",
+    "url": "/AirBnb2/static/js/2.69773152.chunk.js"
+  },
+  {
+    "revision": "ddbcd728b85ee9338197",
+    "url": "/AirBnb2/static/js/runtime~main.ddbcd728.js"
+  },
+  {
+    "revision": "56477b5e5024f02a32c51b69c9ad5b08",
+    "url": "/AirBnb2/static/media/image-12.56477b5e.png"
+  },
+  {
+    "revision": "a6ccb5cd0b47360acada9a9832e2eb66",
+    "url": "/AirBnb2/static/media/wedding-photography.a6ccb5cd.png"
+  },
+  {
+    "revision": "de05b627243a00c8f4f4d9ad09812f4c",
+    "url": "/AirBnb2/static/media/Group-77.de05b627.png"
+  },
+  {
+    "revision": "73c0ef5714f09194268e9e1ff1c2366a",
+    "url": "/AirBnb2/static/media/mountain-bike.73c0ef57.png"
+  },
+  {
+    "revision": "1ef5ba5fd0c73223494bfd0e80b4532a",
+    "url": "/AirBnb2/index.html"
+  }
+];
